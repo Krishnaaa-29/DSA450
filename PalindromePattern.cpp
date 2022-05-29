1829fb43cf1a09d3d53513e76bd7ce3e32ce084c
Krishna Kulshreshtha <75928481+Krishnaaa-29@users.noreply.github.com>
2022-05-29 12:58:41 +0530

    Palindrome

--- PalindromePattern.cpp
+++ PalindromePattern.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-
     int n;
     cin>>n; 
-
     for (int i = 1; i <= n; i++)
     {
         for (int j= 0; j < n - i; j++)
         {
             cout<<" ";
         }
-
         int count = i;
-
         for (int j = n - i; j < n; j++)
         {
             cout<<count;
             count--;
         }
-
         count = 2;
-
         for (int j = n; j < n + i - 1; j++)
         {
             cout<<count;
             count++;
         }
-        
         cout<<endl;
-
     }
-    
     return 0;
-
 }
